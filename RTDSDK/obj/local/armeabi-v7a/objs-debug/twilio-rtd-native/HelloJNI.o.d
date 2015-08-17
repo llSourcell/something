d1fbec09cdf0fa814fd88e3a9412ca887c5c272e
Kumkum Bagchi <kbagchi@twilio.com>
2015-08-17 13:49:59 -0700

    Re-factoring

---
+++ RTDSDK/obj/local/armeabi-v7a/objs-debug/twilio-rtd-native/HelloJNI.o.d
@@ -0,0 +1,92 @@
+/Users/kbagchi/projects/RTD/workspace/obj/local/armeabi-v7a/objs-debug/twilio-rtd-native/HelloJNI.o: \
+ /Users/kbagchi/projects/RTD/workspace/jni/HelloJNI.cpp \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/stdio.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/stdint.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/limits.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/wchar.h \
+ /Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/wctype.h \
+ /Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/xlocale.h \
+ /Users/kbagchi/projects/RTD/workspace/jni/HelloJNI.h
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/stdio.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/stdint.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/limits.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/wchar.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/wctype.h:
+
+/Users/kbagchi/Software/android-ndk-r10d/sources/cxx-stl/llvm-libc++/../../android/support/include/xlocale.h:
+
+/Users/kbagchi/projects/RTD/workspace/jni/HelloJNI.h:
