9468576c1810b37d98e33a8ddd20a143c42ad5a6
Kumkum Bagchi <kbagchi@twilio.com>
2015-09-21 13:38:15 -0700

    Added a test file to trigger Jenkins job

---
+++ ip-messaging-demo-android/src/com/twilio/ipmessaging/demo/DemoChannelListener.java
@@ -0,0 +1,53 @@
+package com.twilio.ipmessaging.demo;
+
+import java.util.Map;
+
+import com.twilio.ipmessaging.ChannelListener;
+import com.twilio.ipmessaging.Member;
+import com.twilio.ipmessaging.Message;
+
+public class DemoChannelListener implements ChannelListener {
+	
+	@Override
+	public void onMessageAdd(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMessageChange(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMessageDelete(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMemberJoin(Member member) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMemberChange(Member member) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMemberDelete(Member member) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAttributesChange(Map<String, String> updatedAttributes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
