9a28db38797a687dd905fc9772e18d8a380ceba8
Kumkum Bagchi <kbagchi@twilio.com>
2015-11-12 20:16:18 -0800

    Removed hard coded url for the release

--- ip-messaging-demo-android/src/com/twilio/ipmessaging/demo/LoginActivity.java
+++ ip-messaging-demo-android/src/com/twilio/ipmessaging/demo/LoginActivity.java
@@ -21,14 +21,8 @@ import android.widget.Toast;
 
 public class LoginActivity extends Activity implements LoginListener {
 	private static final Logger logger = Logger.getLogger(LoginActivity.class);
-	public  boolean DevEnvV2 = false; 
-	
-	//test with shared instance 
-	//private static final String AUTH_PHP_SCRIPT = "https://twilio-ip-messaging-token.herokuapp.com/token?ttl=999999&account_sid=AC96ccc904753b3364f24211e8d9746a93&auth_token=647e1a16c9e5285b4188ca36e4aca150&service_sid=IS388ffb847f314e289175d682cc24cd72&identity=";
-	//PROD TDL
-	private static final String AUTH_PHP_SCRIPT = "https://twilio-ip-messaging-token.herokuapp.com/token?ttl=999999&account_sid=AC96ccc904753b3364f24211e8d9746a93&auth_token=647e1a16c9e5285b4188ca36e4aca150&service_sid=IS2f100be24f76492abdc40352aa22e367";
-	
-	//private static final String AUTH_PHP_SCRIPT = "http://companyfoo.com/token";
+		
+	private static final String AUTH_PHP_SCRIPT = "http://companyfoo.com/token";
 	
 	private static final String DEFAULT_CLIENT_NAME = "TestUser";
 	private ProgressDialog progressDialog;
